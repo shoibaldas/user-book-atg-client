b89bbcf160053ddb7dfcdf71fa6af5f0e2dd9375
Shoibal Das <shoibalshantu@gmail.com>
2022-12-22 01:41:21 +0600

    user list redered with user data and avatar for each user with first letter of firstname and lastname has been integrated

--- src/pages/Shared/SidbarMenu/SidebarData.js
+++ src/pages/Shared/SidbarMenu/SidebarData.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup'
+import { Link } from 'react-router-dom';
+import Avatar from 'react-avatar';
 
 const SidebarData = ({user}) => {
-    console.log(user)
-    const {Bio} =user;
-    const {username} = user.profile;
+    const {Bio, avatar, jobTitle, createdAt, id} =user;
+    const {username, firstName, lastName, email} = user.profile;
+    console.log(avatar)
     return (
-        <div>
-            <h1 className='text-white'>{username}</h1>
-        </div>
+        <ListGroup>
+            <ListGroup.Item action variant="dark">
+                <Link to={`/user-details-of/${id}`} className="text-decoration-none text-dark">
+                    <div className="d-flex align-items-center">
+                        <div>
+                            <Avatar size="40" round={true} name={firstName+" "+lastName} maxInitials={2}/>
+                        </div>
+                        <div className='mx-2'>
+                            <p style={{margin:'0'}}>{firstName} {lastName}</p>
+                        </div>
+                    </div>
+                </Link>
+            </ListGroup.Item> 
+        </ListGroup>
     );
 };
 
--- src/pages/Shared/SidbarMenu/SidebarMenu.js
+++ src/pages/Shared/SidbarMenu/SidebarMenu.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import SidebarData from './SidebarData';
 import Loading from '../../../components/Loading/Loading';
+import { CgUserList } from "react-icons/cg";
 
 const SidebarMenu = () => {
     const [loading, setLoading] = useState(true);
@@ -21,7 +22,13 @@ const SidebarMenu = () => {
     }
 
     return (
-        <div className="bg-dark p-2" style={{height:'100vh', overflow:'auto', margin:'auto'}}>
+        <div className="bg-dark p-3" style={{height:'100vh', overflow:'auto', margin:'auto'}}>
+            <div className='d-flex'>
+                <CgUserList className='text-primary mb-4' style={{fontSize:'24px'}}></CgUserList>
+                <div className='mx-2'>
+                    <h5 className="text-light">User List</h5>
+                </div>
+            </div>
             {
                 userList?.map(user=> <SidebarData 
                     key={user.id}
